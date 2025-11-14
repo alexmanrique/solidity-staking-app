b008e934efb7aba7664f1d0bb93da47cf7d9869f
alex <amanrique86@gmail.com>
2025-11-14 13:40:46 +0100

    fix format solidity files

--- test/StakingAppTest.t.sol
+++ test/StakingAppTest.t.sol
@@ -6,56 +6,72 @@ import "../src/StakingToken.sol";
 import "../src/StakingApp.sol";
 
 contract StakingAppTest is Test {
-
     StakingToken stakingToken;
     StakingApp stakingApp;
     string name_ = "Staking Token";
     string symbol_ = "STK";
     uint256 stakingPeriod_ = 100000000000000; // 30 days
-    uint256 fixedStakingAmount_ = 100 ether;  
-    uint256 rewardPerPeriod_ = 5 ether; 
+    uint256 fixedStakingAmount_ = 100 ether;
+    uint256 rewardPerPeriod_ = 5 ether;
     address randomUser = vm.addr(2);
     address owner_ = vm.addr(1);
-  
-  
-  function setUp() public {
-     stakingToken = new StakingToken(name_, symbol_);   
-     stakingApp = new StakingApp(address(stakingToken), owner_, stakingPeriod_, fixedStakingAmount_, rewardPerPeriod_); 
-  }
 
-  function testStakingTokenCorrectlyDeployed() public view {
-      assert(address(stakingToken) != address(0));
-  }
+    function setUp() public {
+        stakingToken = new StakingToken(name_, symbol_);
+        stakingApp =
+            new StakingApp(address(stakingToken), owner_, stakingPeriod_, fixedStakingAmount_, rewardPerPeriod_);
+    }
 
-  function testStakingAppCorrectlyDeployed() public view {
-      assert(address(stakingApp) != address(0));
-  }
+    function testStakingTokenCorrectlyDeployed() public view {
+        assert(address(stakingToken) != address(0));
+    }
 
-  function testShouldRevertIfNotOwnerTriesToChangeStakingPeriod() public {  
-      vm.startPrank(randomUser);
-      uint256 newStakingPeriod = 60 days;
-      vm.expectRevert();
-      stakingApp.changeStakingPeriod(newStakingPeriod);
-      vm.stopPrank();
-  }
+    function testStakingAppCorrectlyDeployed() public view {
+        assert(address(stakingApp) != address(0));
+    }
 
-  function testShouldChangeStakingPeriod() public {
-    vm.startPrank(owner_);
-    uint256 newStakingPeriod = 60 days;
-    stakingApp.changeStakingPeriod(newStakingPeriod);
-    assert(stakingApp.stakingPeriod() == newStakingPeriod);
-    vm.stopPrank();
-  }
+    function testShouldRevertIfNotOwnerTriesToChangeStakingPeriod() public {
+        vm.startPrank(randomUser);
+        uint256 newStakingPeriod = 60 days;
+        vm.expectRevert();
+        stakingApp.changeStakingPeriod(newStakingPeriod);
+        vm.stopPrank();
+    }
 
-  function testContractReceiveEtherCorrectly() public {
-    vm.startPrank(randomUser);
-    uint256 amount = 1 ether;
-    vm.deal(randomUser, amount);
-    uint256 balanceBefore = address(stakingApp).balance;
-    (bool success,)= address(stakingApp).call{value:amount}("");
-    uint256 balanceAfter = address(stakingApp).balance;
-    assert(balanceAfter - balanceBefore == amount);
-    assert(success);
-    vm.stopPrank();
-  }
+    function testShouldChangeStakingPeriod() public {
+        vm.startPrank(owner_);
+        uint256 newStakingPeriod = 60 days;
+        stakingApp.changeStakingPeriod(newStakingPeriod);
+        assert(stakingApp.stakingPeriod() == newStakingPeriod);
+        vm.stopPrank();
+    }
+
+    function testContractReceiveEtherCorrectly() public {
+        vm.startPrank(randomUser);
+        uint256 amount = 1 ether;
+        vm.deal(randomUser, amount);
+        uint256 balanceBefore = address(stakingApp).balance;
+        (bool success,) = address(stakingApp).call{value: amount}("");
+        uint256 balanceAfter = address(stakingApp).balance;
+        assert(balanceAfter - balanceBefore == amount);
+        assert(success);
+        vm.stopPrank();
+    }
+
+    function testDepositIncorrectAmountShouldRevert() public {
+        vm.startPrank(randomUser);
+        uint256 amount = 1 ether;
+        vm.expectRevert();
+        stakingApp.depositTokens(amount);
+        vm.stopPrank();
+    }
+
+    function testShouldDepositTokensCorrectly() public {
+        vm.startPrank(randomUser);
+        vm.deal(randomUser, fixedStakingAmount_);
+        uint256 amount = fixedStakingAmount_;
+        stakingApp.depositTokens(amount);
+        vm.stopPrank();
+        assert(stakingApp.userBalance(randomUser) == amount);
+    }
 }
--- test/StakingTokenTest.t.sol
+++ test/StakingTokenTest.t.sol
@@ -6,30 +6,27 @@ import "../src/StakingToken.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract StakingTokenTest is Test {
+    StakingToken stakingToken;
+    string name_ = "Staking Token";
+    string symbol_ = "STK";
+    address randomUser = vm.addr(1);
 
-   StakingToken stakingToken;
-   string name_ = "Staking Token";
-   string symbol_ = "STK";
-   address randomUser = vm.addr(1);
+    function setUp() public {
+        stakingToken = new StakingToken(name_, symbol_);
+    }
 
-   function setUp() public{
-       stakingToken = new StakingToken(name_, symbol_);
-   }
+    function testStakingTokenMintsCorrectly() public {
+        uint256 amount_ = 1 ether;
 
-   function testStakingTokenMintsCorrectly() public {
-       
-       uint256 amount_ = 1 ether;
-       
-       vm.startPrank(randomUser);
-       // Token balance previous 
-       uint256 balanceBefore_ = IERC20(address(stakingToken)).balanceOf(randomUser);
-       stakingToken.mint(amount_);
-       
-       // Token balance after 
-       uint256 balanceAfter_ = IERC20(address(stakingToken)).balanceOf(randomUser);
-       
-       assert(balanceAfter_ - balanceBefore_ == amount_);
-       vm.stopPrank();
-   }
+        vm.startPrank(randomUser);
+        // Token balance previous
+        uint256 balanceBefore_ = IERC20(address(stakingToken)).balanceOf(randomUser);
+        stakingToken.mint(amount_);
 
+        // Token balance after
+        uint256 balanceAfter_ = IERC20(address(stakingToken)).balanceOf(randomUser);
+
+        assert(balanceAfter_ - balanceBefore_ == amount_);
+        vm.stopPrank();
+    }
 }
